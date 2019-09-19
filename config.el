c20a2d18130094144c10e1f2e2c1db610f0816e2
Zach Leong <earthospreyhd@gmail.com>
2019-09-19 21:44:32 +1000

    Changed font

--- config.el
+++ config.el
@@ -4,9 +4,11 @@
 ;; (exec-path-from-shell-initialize)
 (require 'doom-themes)
 (global-visual-line-mode t)
-;; (setq-default word-wrap t)
-(setq doom-font (font-spec :family "iosevka ss04" :size 18 :weight 'light))
+(setq doom-font (font-spec :family "Victor Mono" :size 16 :style "regular"))
+;; (setq doom-font (font-spec :family "iosevka ss04" :size 18 :weight 'light))
+;; (setq doom-font (font-spec :family "monoid" :size 15 :style "retina"))
 ;; (setq doom-font (font-spec :family "iosevka ss04" :size 17))
+;; (setq doom-font (font-spec :family "fira code" :size 16))
 (setq doom-themes-enable-bold nil
       doom-themes-enable-italic nil)
 
